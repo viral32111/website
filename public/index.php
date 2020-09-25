ee516beaa646284525827e5d71ae8d24a7409494
viral32111 <19510403+viral32111@users.noreply.github.com>
2020-09-25 18:27:44 +0100

    Configure logging

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
+<?php
+// Set the name of the session cookie
+session_name( '__Secure-ID' );
+
+// Set the options of the session cookie
+session_set_cookie_params( [
+
+	// Last for 10 years
+	'lifetime' => 315576000,
+
+	// Send the cookie to every path
+	'path' => '/',
+
+	// Send the cookie to all of this server
+	'domain' => '.' . $_SERVER[ 'SERVER_NAME' ],
+
+	// Only send over secure connections
+	'secure' => true,
+
+	// Prevent JavaScript from accessing this cookie
+	'httponly' => true,
+
+	// Strictly only send to the same site
+	'samesite' => 'Strict'
+
+] );
+
+// Start the session for the client
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 	<head>
